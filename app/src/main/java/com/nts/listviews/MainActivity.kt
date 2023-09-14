e37a3f06c7d4a86b91cfcf5929db85ed96634647
NTS-AkshayBokhare <akshay.bokhare@neutrinotechlabs.com>
2023-09-14 18:49:40 +0530

    ListViews

---
+++ app/src/main/java/com/nts/listviews/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.nts.listviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listView:ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        listView=findViewById(R.id.listView)
+
+        var countryList=resources.getStringArray(R.array.Countries)
+        var arrayAdapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,countryList)
+        listView.adapter=arrayAdapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+           val countryName: String= parent.getItemAtPosition(position).toString()
+
+            Toast.makeText(applicationContext,
+                "You selected The " +countryName,
+                Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
