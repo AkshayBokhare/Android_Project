c9d1cbe0d69663e196ecde755873f2a3177f8f28
Akshay Bokhare <138112705+NTS-AkshayBokhare@users.noreply.github.com>
2023-09-19 18:30:58 +0530

    Delete DemoFile.kt

--- DemoFile.kt
+++
@@ -1,9 +0,0 @@
-import android.widget.TextView
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setListeners()
-    }
-}
